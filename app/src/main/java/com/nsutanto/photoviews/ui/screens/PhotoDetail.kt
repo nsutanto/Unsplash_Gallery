00301f74edb3c948946375aa1ea45a045e84139d
nsutanto <nicholas.sutanto@gmail.com>
2025-07-04 07:25:30 -0500

    Change photo object to photo url list

--- app/src/main/java/com/nsutanto/photoviews/ui/screens/PhotoDetail.kt
+++ app/src/main/java/com/nsutanto/photoviews/ui/screens/PhotoDetail.kt
@@ -1,25 +1,17 @@
 package com.nsutanto.photoviews.ui.screens
 
-import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.CircularProgressIndicator
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.nsutanto.photoviews.viewmodel.PhotoDetailViewModel
@@ -29,10 +21,9 @@ import org.koin.androidx.compose.koinViewModel
 fun PhotoDetail(viewModel: PhotoDetailViewModel = koinViewModel(),
                 photoId: String) {
 
-    val photoList by viewModel.photos.collectAsStateWithLifecycle()
+    val photoUrls by viewModel.photoListUrl.collectAsStateWithLifecycle()
 
-    if (photoList.isEmpty()) {
-        // You can show a loading indicator or blank screen
+    if (photoUrls.isEmpty()) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             CircularProgressIndicator()
         }
@@ -40,13 +31,15 @@ fun PhotoDetail(viewModel: PhotoDetailViewModel = koinViewModel(),
     }
 
     // Find the initial page based on photoId
-    val initialPage = remember(photoId to photoList) {
+    val initialPage = remember(photoId to photoUrls) {
         viewModel.getPhotoIndexById(photoId)
     }
 
     val pagerState = rememberPagerState(
         initialPage = initialPage,
-        pageCount = { photoList.size }
+        pageCount = {
+            photoUrls.size
+        }
     )
 
     // Handle back press and return current photo id
@@ -61,39 +54,14 @@ fun PhotoDetail(viewModel: PhotoDetailViewModel = koinViewModel(),
         state = pagerState,
         modifier = Modifier.fillMaxSize()
     ) { page ->
-        val photo = photoList[page]
         AsyncImage(
-            model = photo.urls?.regular,
+            model = photoUrls[page],
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
     }
-
 }
-/*
-    // Display the photo
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black),
-        contentAlignment = Alignment.Center
-    ) {
-        photoDetail.url?.let { photoUrl ->
-            AsyncImage(
-                model = photoUrl,
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            )
-        } ?: run {
-            // To do: handle error
-        }
-    }
-
- */
 
 
 
--- app/src/main/java/com/nsutanto/photoviews/viewmodel/PhotoDetailViewModel.kt
+++ app/src/main/java/com/nsutanto/photoviews/viewmodel/PhotoDetailViewModel.kt
@@ -15,36 +15,11 @@ class PhotoDetailViewModel(private val repository: IPhotoRepository) : ViewModel
         val userName: String? = null,
     )
 
-    private val _photos = MutableStateFlow<List<Photo>>(emptyList())
-    val photos: StateFlow<List<Photo>> = _photos
+    //private val _photos = MutableStateFlow<List<Photo>>(emptyList())
+    //val photos: StateFlow<List<Photo>> = _photos
 
-    private val _photoUrls = MutableStateFlow<List<String>>(emptyList())
-    val photoUrls: StateFlow<List<String>> = _photoUrls
-
-    init {
-        viewModelScope.launch {
-            repository.photoFlow.collect { photoList ->
-                _photos.value = photoList
-            }
-        }
-    }
-
-    fun getPhotoIndexById(photoId: String): Int {
-        return _photos.value.indexOfFirst { it.id == photoId }.coerceAtLeast(0)
-    }
-
-    /*data class PhotoDetail(
-        val url: String? = null,
-        val userName: String? = null,
-    )
-
-
-
-    private val _photoDetailState = MutableStateFlow(PhotoDetail())
-    val photoDetailState: StateFlow<PhotoDetail> = _photoDetailState
-
-    private val _currentIndex = MutableStateFlow(0)
-    val currentIndex: StateFlow<Int> = _currentIndex
+    private val _photoListUrl = MutableStateFlow<List<String>>(emptyList())
+    val photoListUrl: StateFlow<List<String>> = _photoListUrl
 
     private var photoList = mutableListOf<Photo>()
 
@@ -52,25 +27,12 @@ class PhotoDetailViewModel(private val repository: IPhotoRepository) : ViewModel
         viewModelScope.launch {
             repository.photoFlow.collect { photos ->
                 photoList = photos.toMutableList()
+                _photoListUrl.value = photos.mapNotNull { it.urls?.regular }
             }
         }
     }
 
-    fun getPhotoDetail(photoId: String) {
-        _currentIndex.value = photoList.indexOfFirst { it.id == photoId }.coerceAtLeast(0)
-        updatePhotoDetail()
+    fun getPhotoIndexById(photoId: String): Int {
+        return photoList.indexOfFirst { it.id == photoId }.coerceAtLeast(0)
     }
-
-    fun getPhotoUrls(): List<String> {
-        return photoList.mapNotNull { it.urls?.regular }
-    }
-
-    private fun updatePhotoDetail() {
-        val photo = photoList.getOrNull(_currentIndex.value)
-        _photoDetailState.value = PhotoDetail(
-            url = photo?.urls?.regular,
-        )
-    }*/
-
-
 }
