5b19200a2cc9653e13e7d69b73c643dd91ca0850
nsutanto <nicholas.sutanto@gmail.com>
2025-07-05 17:40:53 -0500

    Fix Unit Tests
    - Fix photo dao test
    - Fix photo repository test

--- app/src/main/java/com/nsutanto/photoviews/db/PhotoDao.kt
+++ app/src/main/java/com/nsutanto/photoviews/db/PhotoDao.kt
@@ -4,11 +4,12 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PhotoDao {
     @Query("SELECT * FROM photos")
-    suspend fun getAll(): List<PhotoEntity>
+    fun getAll(): Flow<List<PhotoEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(photos: List<PhotoEntity>)
